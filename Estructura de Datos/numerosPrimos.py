98feccd6e64e58bd053f404d20f7bc55049cbe34
Ruben <rubenduarte47@gmail.com>
2022-09-20 23:18:34 -0300

    Fin numeros primos

---
+++ Estructura de Datos/numerosPrimos.py
@@ -0,0 +1,17 @@
+# Ejercicio 1: Pide un número por teclado e indica si es un número primo o no. 
+# Un número primo es aquel que solo puede dividirse entre 1 y sí mismo. 
+# Por ejemplo: 25 no es primo, ya que 25 es divisible entre 5, sin embargo, 17 si es primo.
+
+numero = int(input("Ingrese un número a evaluar: "))
+cont = 0
+print()
+print("{0} es divisible por".format(numero), end=": ")
+for n in range(1, numero+1):
+  if numero % n == 0:
+    print(n, end=" - ")
+    cont += 1
+print("Fin")
+if cont == 2:
+  print("El número ingresado si es primo, tiene {0} divisores".format(cont))
+else:
+  print("El número ingresado no es primo, tiene {0} divisores".format(cont))
